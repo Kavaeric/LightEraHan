e4e5223c825848a41abd1a352047a8d73bcf36ec
Kavaeric <kavaeric@gmail.com>
2024-05-12 01:12:57 -0400

    Added lookup tables and styling

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import './App.css';
 import kuromoji from "kuromoji/build/kuromoji";
 import { useEffect, useState, useRef } from 'react';
 import OutputTokenArray from './OutputTokenArray';
+import Papa from "papaparse";
 
 function App() {
 
 	// Container variable for tokenizer system
 	const [tokenizer, setTokenizer] = useState(null);
-	const [inputParse, setInputParse] = useState(null);
+
+	// Containers for dictionaries
+	const [ctParticles, SetCTParticles] = useState(null);
+
+	// Containers for the pipeline
+	const [outputArrays, setOutputArrays] = useState(null); // Final output containing every step
+	const [outputSteps, setOutputSteps] = useState(null); // Array of strings describing each step, for writing headers in the DOM
 	const inputField = useRef(null);
 
 	// Will run any enclosed function when a dependency changes
-	// For most cases that's on startup
+	// This one has no dependencies so it'll run once on startup
 	useEffect(() => {
 
 		// Initialise the tokenizer
@@ -26,32 +33,82 @@ function App() {
 			console.log("Tokenizer initialised.");
 			console.log("- - - - -");
 		});
+
+		// Conversion tables parsed from .CSVs
+		Papa.parse("contables/particles.csv", {
+			download: true,
+			header: true,
+			comments: "//",
+
+			complete: function(result) {
+				console.log("Parsed particles.csv");
+				console.log(result);
+				SetCTParticles(result.data);
+			}
+		});
 	// Defining dependencies, of which there aren't any, hence the empty array
 	// This won't change, though, so it'll just run once
 	},[]);
 
 	// Whenever inputParse changes, log it
 	useEffect(() => {
-		console.log(inputParse);
-	},[inputParse]);
+		console.log(outputArrays);
+	},[outputArrays]);
+
+	function highlightTokens(wordPosition) {
+		console.log(wordPosition);
+	}
+
+	// Handler for the "copy" button on each output row
+	function copyOutputText(outputIndex) {
+		let copiedText = outputArrays[outputIndex + 1].map(token => token.display_form);
+		navigator.clipboard.writeText(copiedText.join(""));
+		console.log("Copied " + copiedText.join("") + " to clipboard.");
+	}
 
 	function handleSubmit(event) {
-
 		// The mark of a universe governed by an uncaring God
 		event.preventDefault();
+		console.log("- - - - -");
 
 		if (!tokenizer) {
 			console.log("Tokenizer not loaded!");
 			return;
 		}
 
+		// This will be the overarching super-array that holds all the steps
+		// At the end of the process the data will be pushed through to outputArrays
+		let tokenArrays = [];
+		let pipelineSteps = [];
+
 		// Go with current.value if null; otherwise fallback to the placeholder
 		let inputText = inputField.current.value||inputField.current.placeholder;
-		setInputParse(tokenizer.tokenize(inputText));
-
 		console.log("Submitted: " + inputText);
-		
+
+		// setInputParse(tokenizer.tokenize(inputText));
+		tokenArrays[0] = tokenizer.tokenize(inputText);
 		console.log("Initial parse complete.");
+		
+		// Add a new key called "display form". This is what will be shown in the client
+		// and allows the rest of the kuromoji token properties to be preserved
+		for (let token of tokenArrays[0]) {
+			token.display_form = token.surface_form;
+		}
+		console.log(tokenArrays[0]);
+
+		// STEP 1: REPLACE PARTICLES
+		tokenArrays.push(structuredClone(tokenArrays[0]));
+		// then run a function that modifies tokenArrays.at(-1)
+		pipelineSteps.push("Replace particles");
+
+		// STEP 2: REMOVE TENSES
+		tokenArrays.push(structuredClone(tokenArrays[0]));
+		// then run a function that changes all verbs to show their basic form
+		pipelineSteps.push("Remove tenses");
+
+		// Push the final results to the appropriate containers for render
+		setOutputArrays(tokenArrays);
+		setOutputSteps(pipelineSteps);
 
 		// This won't work as it won't update until next render
 		// console.log(inputParse);
@@ -70,13 +127,36 @@ function App() {
 
 		<div className="output">
 			<div className="outputStep">
-				<h1>Initial parse</h1>
+				<div className="outputStepHeader">
+					<h1>Initial parse</h1>
+					<button onClick={() => copyOutputText(0)} className="outputCopyBtn" disabled={outputArrays === null}>Copy</button>
+				</div>
+				<div className="tokenArrayOutput">
 				{
-					inputParse
-						? <OutputTokenArray tokens={inputParse} />
+					outputArrays
+						? <OutputTokenArray tokens={outputArrays[0]} />
 						: <div className="tokenArrayOutput">No input. Enter some text, and then click the "convert" button.</div>
 				}
+				</div>
 			</div>
+
+			{
+				outputArrays
+					// For every tokenArray in tokenArrays, create a new outputStep div
+					// with its own tokenArrayOutput class.
+					? outputArrays.slice(1).map((tokenArray, index) => 
+						<div className="outputStep" key={index}>
+							<div className="outputStepHeader">
+								<h1>Step {index + 1}: {outputSteps[index]}</h1>
+								<button onClick={() => copyOutputText(index)} className="outputCopyBtn">Copy</button>
+							</div>
+							<div className="tokenArrayOutput" key={index}>
+								<OutputTokenArray tokens={outputArrays[index]} />
+							</div>
+						</div>)
+					// Otherwise, output nothing
+					: ""
+			}
 		</div>
 		</div>
 	);
--- src/OutputToken.js
+++ src/OutputToken.js
@@ -1,10 +1,51 @@
 import classNames from "classnames";
 
 export default function OutputToken ({token}) {
+
+    // TODO: Move this from this file to App.js
+    function unhighlightTokens() {
+        // Remove the highlight from all other items
+        const unhighlightTargets = document.getElementsByClassName("tokenOutput");
+
+        // Remove the highlight class from all those items
+        for (let outputToken of unhighlightTargets) {
+            outputToken.classList.remove("tokenHighlighted");
+        }
+    }
+
+    // This too
+    function handleClick(event) {
+
+        // If it's not highlighted, highlight it; otherwise, just unhighlight everything
+        if (!event.target.classList.contains("tokenHighlighted")) {
+            console.log("[" + token.word_position + "] " + token.surface_form);
+            console.log(token);
+            unhighlightTokens();
+
+            // Find all other tokens with the same token "ID"
+            const highlightTargets = document.getElementsByClassName("token_" + token.word_position);
+            
+            // Add the highlighted token class
+            for (let outputToken of highlightTargets) {
+                outputToken.classList.add("tokenHighlighted");
+            }
+        } else {
+            unhighlightTokens();
+        }
+    }
+
     return (
-        <span className={
+        <a className={
             classNames("tokenOutput", 
-            {"tokenParticle": token.pos === "助詞"})}>
-        {token.surface_form}</span>
+            {"tokenNoun": token.pos === "名詞" && token.pos_detail_1 != "固有名詞"}, // Excluding proper nouns
+            {"tokenParticle": token.pos === "助詞"},
+            {"tokenVerb": token.pos === "動詞"},
+            {"tokenVerbAux": token.pos === "助動詞"},
+            {"tokenInterjection": token.pos === "感動詞"},
+            {"tokenAdjective": token.pos === "形容詞"},
+            {"tokenSymbol": token.pos === "記号"})
+            + " token_" + token.word_position}
+            href="#" onClick={handleClick}>
+        {token.surface_form}</a>
     );
 }
